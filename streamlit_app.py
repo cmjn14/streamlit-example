976cbc220d3d0a6542d8845b26303866a4ea9ba5
cmjn14 <cedric.lopez@free.fr>
2023-05-25 15:57:31 +0200

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,54 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+def render_basic_tree():
+    with open("./data/flare.json", "r") as f:
+        data = json.loads(f.read())
+
+    for idx, _ in enumerate(data["children"]):
+        data["children"][idx]["collapsed"] = idx % 2 == 0
+
+    option = {
+        "tooltip": {"trigger": "item", "triggerOn": "mousemove"},
+        "series": [
+            {
+                "type": "tree",
+                "data": [data],
+                "top": "1%",
+                "left": "7%",
+                "bottom": "1%",
+                "right": "20%",
+                "symbolSize": 7,
+                "label": {
+                    "position": "left",
+                    "verticalAlign": "middle",
+                    "align": "right",
+                    "fontSize": 9,
+                },
+                "leaves": {
+                    "label": {
+                        "position": "right",
+                        "verticalAlign": "middle",
+                        "align": "left",
+                    }
+                },
+                "emphasis": {"focus": "descendant"},
+                "expandAndCollapse": True,
+                "animationDuration": 550,
+                "animationDurationUpdate": 750,
+            }
+        ],
+    }
+    st_echarts(option, height="500px")
+
+
+ST_TREE_DEMOS = {
+    "Tree: From Left to Right Tree": (
+        render_basic_tree,
+        "https://echarts.apache.org/examples/en/editor.html?c=tree-basic",
+    ),
+}
+
 def render_gentree():
     option = {
         "title": {"text": "Arbre généalogique"},
