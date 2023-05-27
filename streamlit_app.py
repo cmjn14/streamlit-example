75f7923d0b3e2942fcbbceefc393f680b4d4730a
cmjn14 <cedric.lopez@free.fr>
2023-05-27 09:29:05 +0200

    Update streamlit_app.py
    info

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,10 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+# Calcul distances et angle à partir des coordonnées : https://www.movable-type.co.uk/scripts/latlong.html
+# Python GEDCOM parser : https://pypi.org/project/python-gedcom/
+
+
 def render_scatter():
     hours = ['12a', '1a', '2a', '3a', '4a', '5a', '6a', '7a', '8a', '9a','10a','11a','12p', '1p', '2p', '3p', '4p', '5p', '6p', '7p', '8p', '9p', '10p', '11p']
     days = ['Saturday', 'Friday', 'Thursday', 'Wednesday', 'Tuesday', 'Monday', 'Sunday']
